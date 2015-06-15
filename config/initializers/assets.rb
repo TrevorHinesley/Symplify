5c31bc82e77e2402e3bb1af17eab21599699f8e3
Trevor Hinesley <trevor.hinesley@gmail.com>
2015-06-14 23:45:35 -0500

    Initial site

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = '1.1'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
@@ -9,3 +9,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "template", "javascripts")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "template", "stylesheets")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "images")
+Rails.application.config.assets.precompile += %w( *.eot *.svg *.ttf *.woff *.png *.jpg *.jpeg *.gif template/* vendor/assets/javascripts/* vendor/assets/stylesheets/* vendor/assets/template/stylesheets/* vendor/assets/template/javascripts/* )
